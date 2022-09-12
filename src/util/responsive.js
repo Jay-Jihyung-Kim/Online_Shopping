673b34937849a1d58209ef5bb2eaa4eb16e411a2
Jay Kim <kakarock0124@gmail.com>
2022-09-12 02:49:57 -0700

    Adjusted mobile max-resolution

--- src/util/responsive.js
+++ src/util/responsive.js
@@ -9,7 +9,7 @@ export const small = (props) => {
 };
 export const mobile = (props) => {
   return css`
-    @media only screen and (max-width: 790px) {
+    @media only screen and (max-width: 990px) {
       ${props}
     }
   `;
